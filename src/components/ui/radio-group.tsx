fe82b32e6b51c0081d1019be4bec50ffb8aec2b8
NEO <122967068+Krishnachaitanya-dev@users.noreply.github.com>
2025-04-05 16:29:19 +0530

    Update radio-group.tsx

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -1,50 +1,9 @@
 
-// import * as React from "react"
-// import { CheckIcon } from "@radix-ui/react-icons"
-// import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
+import * as React from "react"
+import { CheckIcon } from "@radix-ui/react-icons"
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 
-// import { cn } from "@/lib/utils"
-
-// const RadioGroup = React.forwardRef<
-//   React.ElementRef<typeof RadioGroupPrimitive.Root>,
-//   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
-// >(({ className, ...props }, ref) => {
-//   return (
-//     <RadioGroupPrimitive.Root
-//       className={cn("grid gap-2", className)}
-//       {...props}
-//       ref={ref}
-//     />
-//   )
-// })
-// RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
-
-// const RadioGroupItem = React.forwardRef<
-//   React.ElementRef<typeof RadioGroupPrimitive.Item>,
-//   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
-// >(({ className, ...props }, ref) => {
-//   return (
-//     <RadioGroupPrimitive.Item
-//       ref={ref}
-//       className={cn(
-//         "aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-//         className
-//       )}
-//       {...props}
-//     >
-//       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-//         <CheckIcon className="h-3.5 w-3.5 fill-primary" />
-//       </RadioGroupPrimitive.Indicator>
-//     </RadioGroupPrimitive.Item>
-//   )
-// })
-// RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
-
-// export { RadioGroup, RadioGroupItem }
-import * as React from "react";
-import { FaCheck } from "react-icons/fa";
-import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 const RadioGroup = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Root>,
@@ -56,9 +15,9 @@ const RadioGroup = React.forwardRef<
       {...props}
       ref={ref}
     />
-  );
-});
-RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+  )
+})
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
@@ -74,11 +33,11 @@ const RadioGroupItem = React.forwardRef<
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <FaCheck className="h-3.5 w-3.5 fill-primary" />
+        <CheckIcon className="h-3.5 w-3.5 fill-primary" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
-  );
-});
-RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName;
+  )
+})
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
 
-export { RadioGroup, RadioGroupItem };
+export { RadioGroup, RadioGroupItem }
